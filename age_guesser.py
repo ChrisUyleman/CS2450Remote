1ba1a224d4847ab6cea336256198d44348954b4a
root <root@Rambobskies-PC2.>
2026-02-08 22:40:42 -0700

    Merge conflict fix

--- age_guesser.py
+++ age_guesser.py
@@ -9,7 +9,11 @@ while UserAnswer != "y":
     RandomChoice = random.randint(15,30)
     UserAnswer = input(f'I think your age is: {RandomChoice} years old. Am I right? (y/n)').strip().lower()
     if UserAnswer == "y":
+<<<<<<< HEAD
         break    
+=======
+        break
+>>>>>>> 225a198 (initial commit + journal)
     print('Rats.')
 
 print(f'Nice to meet you, {Username}!')
